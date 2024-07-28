dff400a19515cf0b1346eb8eb4937b100ffe1d6c
juzex <juzex3@icloud.com>
2024-07-28 21:28:16 +0300

    Delete docs/script.js

--- docs/script.js
+++
@@ -1,9 +0,0 @@
-let time = 60;
-    const timer1 = setInterval(() => { const countdownElement = document.getElementById('countdown1');
-        if(time >= 0) {countdownElement.value = time--;} 
-        else { clearInterval(timer1); }
-}, 1000);
-
-const button = document.querySelector('#startBtn');
-
-button.addEventListener('click', timer1);
