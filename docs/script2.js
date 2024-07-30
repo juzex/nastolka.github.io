f8a00b98adb97d52889d563615abf8e9516f8075
juzex <juzex3@icloud.com>
2024-07-30 19:41:07 +0300

    Update script2.js

--- docs/script2.js
+++ docs/script2.js
@@ -1,5 +1,44 @@
+var color = 'green';
+var a;
+a = 0;
 
-var btn = document.getElementById("button");
-btn.addEventListener("click", function() {
-  this.classList.add("active");
-});
+function change1(identifier) {
+    identifier.style.background = color;
+    a = a + 1;
+    document.getElementById('a').innerText = a;
+}
+
+function change2(identifier) {
+    identifier.style.background = color;
+    a = a + 1;
+    document.getElementById('a').innerText = a;
+}
+
+function change3(identifier) {
+    identifier.style.background = color;
+    a = a + 1;
+    document.getElementById('a').innerText = a;
+}
+
+function change4(identifier) {
+    identifier.style.background = color;
+    a = a + 1;
+    document.getElementById('a').innerText = a;
+}
+
+function change5(identifier) {
+    identifier.style.background = color;
+    a = a + 1;
+    document.getElementById('a').innerText = a;
+}
+
+function change6(identifier) {
+    identifier.style.background = color;
+    a = a + 1;
+    document.getElementById('a').innerText = a;
+}
+
+function reset() {
+    a = 0;
+    document.getElementById('a').innerText = a;
+}
