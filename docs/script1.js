b96419e41b280af487cf301a5a9c2738e11ca5a9
juzex <juzex3@icloud.com>
2024-07-30 19:40:54 +0300

    Update script1.js

--- docs/script1.js
+++ docs/script1.js
@@ -1,10 +1,11 @@
-let time = 60;
+var time = 5;
     const timer1 = setInterval(() => { const countdownElement = document.getElementById('countdown1');
-        if(time >= 0) {countdownElement.value = time--;} 
-        else { clearInterval(timer1); }
+        if(time >= 0) {countdownElement.value = time - 1;} 
+        else { 
+          clearInterval(timer1);
+          console.log('все');
+         }
 }, 1000);
 
-const button = document.querySelector('#startBtn');
-
+const button = document.getElementById('startBtn');
 button.addEventListener('click', timer1);
-
