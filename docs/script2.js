846d2b386ebaf3bd6fb947a97317158985b6563e
juzex <juzex3@icloud.com>
2024-08-09 17:18:51 +0300

    Update script2.js

--- docs/script2.js
+++ docs/script2.js
@@ -4,14 +4,14 @@ let words = ["ноготь", "свидание", "кал", "носок", "ско
 
 var color = 'green';
 var color2 = 'white';
-var check = 0;
+let check = 0;
 var clicked1 = false;
 var clicked2 = false;
 var clicked3 = false;
 var clicked4 = false;
 var clicked5 = false;
 var clicked6 = false;
-
+document.getElementById('a').innerText = check;
 
 function change1(identifier) {
     if (!clicked1) {
@@ -61,7 +61,11 @@ function change6(identifier) {
         document.getElementById('a').innerText = check;
 }}
 
-
+if (clicked1 == clicked2 == clicked3 == clicked4 == clicked5 == clicked6 == true) {
+    document.getElementById('stoptimer').style.display = "none";
+    document.getElementById('starttimer').style.display = "none";
+    document.getElementById('resetcart').style.display = "inline";
+}
 
 
 
@@ -72,7 +76,7 @@ function reset() {
 
 
 
-
+ 
 
 
 
@@ -84,12 +88,29 @@ function reset() {
     d = Math.round(Math.random()*(words.length - 1))
     e = Math.round(Math.random()*(words.length - 1))
     f = Math.round(Math.random()*(words.length - 1))
+    while (a === b || a === c || a === d || a === e || a === f || b === c || b === d || b === e || b === f || c === d || c === e || c === f || d === e || d === f ||  e === f) {
+        a = Math.round(Math.random()*(words.length - 1));
+        b = Math.round(Math.random()*(words.length - 1));
+        c = Math.round(Math.random()*(words.length - 1));
+        d = Math.round(Math.random()*(words.length - 1));
+        e = Math.round(Math.random()*(words.length - 1));
+        f = Math.round(Math.random()*(words.length - 1));
+        console.log("...")
+    }
     document.getElementById('word1').innerText = words[a];
     document.getElementById('word2').innerText = words[b];
     document.getElementById('word3').innerText = words[c];
     document.getElementById('word4').innerText = words[d];
     document.getElementById('word5').innerText = words[e];
     document.getElementById('word6').innerText = words[f];
+    words.splice(a,1);
+    words.splice(b,1);
+    words.splice(c,1);
+    words.splice(d,1);
+    words.splice(e,1);
+    words.splice(f,1);
+    console.log(words);
+    
 
 function resetgame(identifier) {
     clicked1 = false;
@@ -106,16 +127,38 @@ function resetgame(identifier) {
     document.getElementById('cartbtn6').style.background = color2;
     document.getElementById('starttimer').style.display = "inline";
     document.getElementById('resetcart').style.display = "none";
-    a = Math.round(Math.random()*(words.length - 1))
-    b = Math.round(Math.random()*(words.length - 1))
-    c = Math.round(Math.random()*(words.length - 1))
-    d = Math.round(Math.random()*(words.length - 1))
-    e = Math.round(Math.random()*(words.length - 1))
-    f = Math.round(Math.random()*(words.length - 1))
+
+
+
+    a = Math.round(Math.random()*(words.length - 1));
+    b = Math.round(Math.random()*(words.length - 1));
+    c = Math.round(Math.random()*(words.length - 1));
+    d = Math.round(Math.random()*(words.length - 1));
+    e = Math.round(Math.random()*(words.length - 1));
+    f = Math.round(Math.random()*(words.length - 1));
+    while (a === b || a === c || a === d || a === e || a === f || b === c || b === d || b === e || b === f || c === d || c === e || c === f || d === e || d === f ||  e === f) {
+        a = Math.round(Math.random()*(words.length - 1));
+        b = Math.round(Math.random()*(words.length - 1));
+        c = Math.round(Math.random()*(words.length - 1));
+        d = Math.round(Math.random()*(words.length - 1));
+        e = Math.round(Math.random()*(words.length - 1));
+        f = Math.round(Math.random()*(words.length - 1));
+        console.log("...")
+    }
     document.getElementById('word1').innerText = words[a];
     document.getElementById('word2').innerText = words[b];
     document.getElementById('word3').innerText = words[c];
     document.getElementById('word4').innerText = words[d];
     document.getElementById('word5').innerText = words[e];
     document.getElementById('word6').innerText = words[f];
+    words.splice(a,1);
+    words.splice(b,1);
+    words.splice(c,1);
+    words.splice(d,1);
+    words.splice(e,1);
+    words.splice(f,1);
+
+
+
+    
 }
